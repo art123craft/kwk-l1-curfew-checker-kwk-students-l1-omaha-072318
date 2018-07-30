0f3fd7ee35d8ec3b78bc306e7f596c0aa3de67b0
art123craft <cammiebluecammie@gmail.com>
2018-07-30 14:51:36 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,29 @@
 def simple_curfew_checker(time)
   # code goes here
 end
-
-def curfew_checker(time)
-  # code goes here
+if time >= 7 && time < 11
+  		puts "Good morning!"
+elsif time >= 11 && time <17
+ 		puts "Good afternoon!"
+elsif time >= 17 && time < 20        
+ 		puts "Good evening!"
+else
+  		puts "Good night!"
+end
 end
 
-def complex_curfew_checker(time)
-  # code goes here
-end
+# def curfew_checker(time)
+#   # code goes here
+# end
 
-def deluxe_curfew_checker(time)
-  # code goes here
-end
+# def complex_curfew_checker(time)
+#   # code goes here
+# end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+# def deluxe_curfew_checker(time)
+#   # code goes here
+# end
+
+# def platinum_curfew_checker(current_time, curfew_time)
+#   # code goes here
+# end
